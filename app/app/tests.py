5792eb82c2fb437a2614bbb01761e2605c8eea9e
test_seccgeo <hamdi.secilmis@gmail.com>
2026-02-28 20:39:09 +0300

    add test

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests for the application.
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test that the add function returns the correct sum."""
+        res = calc.subtract(10, 5)
+        self.assertEqual(res, 5)
+
+    def test_add_strings(self):
+        """Test that the add function concatenates strings."""
+        res = calc.add('Hello, ', 'World!')
+        self.assertEqual(res, 'Hello, World!')
